77a7ae04768b7e807559ae880e90cbc475364350
Muthukumar Selvarasu <muthukumar.se@gmail.com>
2023-02-03 21:48:07 +0800

    update

--- gke.tf
+++ gke.tf
@@ -14,7 +14,7 @@ variable "gke_num_nodes" {
 }
 
 locals {
-  existing_cluster = google_container_cluster.primary.name != ""
+  existing_cluster = google_container_cluster.primary.*.name
 }
 
 # GKE cluster
@@ -22,6 +22,8 @@ resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
   
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -30,12 +32,8 @@ resource "google_container_cluster" "primary" {
   subnetwork = google_compute_subnetwork.subnet.name
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = length(local.existing_cluster) == 0 ? true : false
   }
-
-  depends_on = [
-    local.existing_cluster
-  ]
 }
 
 # Separately Managed Node Pool
@@ -63,24 +61,3 @@ resource "google_container_node_pool" "primary_nodes" {
     }
   }
 }
-
-
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
-
